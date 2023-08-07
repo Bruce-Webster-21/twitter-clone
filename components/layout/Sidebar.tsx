46ceb4c150100d4bb3fd8d6f16bf9a027c5dc72d
Bruce Webster <brucewebster222@gmail.com>
2023-08-07 16:43:57 -0400

    Added Tweet Btn and Logout Btn to sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,6 +1,9 @@
 import { BsHouseFill, BsBellFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
 import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import SidebarTweetBtn from "./SidebarTweetBtn";
 
 const Sidebar = () => {
   const items = [
@@ -25,6 +28,16 @@ const Sidebar = () => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+          <SidebarTweetBtn />
         </div>
       </div>
     </div>
--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -5,7 +5,10 @@ const SidebarLogo = () => {
   const router = useRouter();
 
   return (
-    <div className="rounded-full h-14 w-14 p-4 flex items-center jusitify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition">
+    <div
+      onClick={() => router.push("/")}
+      className="rounded-full h-14 w-14 p-4 flex items-center jusitify-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition"
+    >
       <BsTwitter size={28} color="white" />
     </div>
   );
